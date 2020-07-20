82f15642d7712a44131d7ff6098a54eb9ec40d51
hamzath-pm21 <68550097+hamzath-pm21@users.noreply.github.com>
2020-07-20 20:29:47 +0530

    made it into three functions

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,32 +1,47 @@
-static bool isbpmOk(float bpm);
-static bool isbpmOk(float bpm)
+static int isbpmOk(float bpm);
+static int isbpmOk(float bpm)
 {
-  bool return_value;
-  return_value = true;
+  int return_value;
+  return_value = 1;
   if(bpm < 70 || bpm > 150) {
-    return_value = false;
+    return_value = 0;
   }
   return (return_value);
 }
 
-static bool isrespRateOk(float respRate) {
-  bool return_value;
-  return_value = true;
+static int isrespRateOk(float respRate) {
+  int return_value;
+  return_value = 1;
   if (respRate < 30 || respRate > 60) {
-    return_value = false;
+    return_value = 0;
   }
   return (return_value);
 }
-  
+
+static int isspo2Ok(float spo2)
+{
+  int return_value;
+  return_value = 1;
+  if (spo2 < 80) {
+    return_value = 0;
+  }
+  return(return_value);
+}
   
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-
-  if(isbpmOk(bpm) != true) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(isrespRateOk(respRate) != true) {
-    return false;
+  int bpmret;
+  int respRateret;
+  int spo2ret;
+  bool retval = false;
+  bpmret = isbpmOk(bpm);
+    
+  spo2ret = isspo2Ok(spo2); 
+    
+  respRateret = isrespRateOk(respRate);
+   
+  if ((bpmret & respRateret & spo2ret) == 1) {
+    retval =  true;
   }
-  return true;
+  return retval;
+  
 }
