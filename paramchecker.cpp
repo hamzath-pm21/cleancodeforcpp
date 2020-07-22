ad8106ad080f5866c1f9aa0f3992c9e43c7bbdb0
hamzath-pm21 <68550097+hamzath-pm21@users.noreply.github.com>
2020-07-22 09:04:36 +0530

    Made IsvitalsOk and IsParamOk part of a class

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,27 +1,25 @@
-static int isparamOk(float bpm, float lower, float upper);
-static int isparamOk(float param, float lower, float upper)
-{
-  int return_value = 1;
-  if ( param < lower || param > upper) {
-    return_value = 0;
-  }
-  return (return_value);
+#include "paramchecker.h"
+
+int health::isParamOk() {
+	int return_value = 1;
+	if (value < lowerbound || value > upperbound) {
+		return_value = 0;
+	}
+	return (return_value);
+
 }
-  
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  int bpmret;
-  int respRateret;
-  int spo2ret;
-  bool retval = false;
-  bpmret = isparamOk(bpm,70,150);
-    
-  spo2ret = isparamOk(spo2,80,100); 
-    
-  respRateret = isparamOk(respRate,30,60);
-   
-  if ((bpmret & respRateret & spo2ret) == 1) {
-    retval =  true;
-  }
-  return retval;
-  
+
+bool Patientreport::IsVitalsOk() {
+	int i;
+	int ret = 0;
+	bool retval = false;
+	for (i = 0; i < TotalReps; i++)
+	{
+		ret += health_rep[i]->isParamOk();
+	}
+	if (ret == TotalReps) {
+		retval = true;
+	}
+	return retval;
 }
+
