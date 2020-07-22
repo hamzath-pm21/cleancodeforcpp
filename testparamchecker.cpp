1f494594ffcb463631e2769bf3d412350123dbb1
hamzath-pm21 <68550097+hamzath-pm21@users.noreply.github.com>
2020-07-22 22:04:25 +0530

    made vector list and objects inserted

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -1,6 +1,10 @@
 #include "paramchecker.h"
 #include <gtest/gtest.h>
 #include <assert.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
  
 /*
 TEST(VitalsTest, BPM) { 
@@ -15,17 +19,33 @@ TEST(VitalsTest, SPO2) {
 
 */
 
-float patdetails[3] = { 100,90,50 };
-float pat2det[3] = { 200,90,50 };
- 
+
 int main(int argc, char **argv) {
 	bool x;
-	Patientreport Pt(&patdetails[0]);
-	Patientreport Pt1(&pat2det[0]);
+	vector<health*> dBVector;
+	bpm b1(100);
+	spo2 sp(90);
+	RespRate rp(50);
+	dBVector.push_back(&b1);
+	dBVector.push_back(&sp);
+	dBVector.push_back(&rp);
+
+	vector<health*> dBVector1;
+	bpm b2(200);
+	spo2 sp2(90);
+	RespRate rp2(50);
+	dBVector1.push_back(&b2);
+	dBVector1.push_back(&sp2);
+	dBVector1.push_back(&rp2);
+
+
+	Patientreport Pt(dBVector);
+	Patientreport Pt1(dBVector1);
 
 	x = Pt.IsVitalsOk();
 	assert(x != false);
 	x = Pt1.IsVitalsOk();
 	assert(x != true);
- return 0;
+
+	return 0;
 }
